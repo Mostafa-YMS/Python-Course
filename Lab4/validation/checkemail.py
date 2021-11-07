f580048a4e0aa7aae667a9f243cc76bf582c33c0
Mostafa-YMS <mostafa_yms@yahoo.com>
2021-11-07 14:29:38 +0200

    Lab 4

--- Lab4/validation/checkemail.py
+++ Lab4/validation/checkemail.py
@@ -3,7 +3,7 @@ import re
 
 def chkmail(text):
     text = text.strip()
-    mail = re.search("[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+", text)
+    mail = re.fullmatch(r"[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+", text)
     if len(text) != 0 and mail is not None:
         return True
     else:
